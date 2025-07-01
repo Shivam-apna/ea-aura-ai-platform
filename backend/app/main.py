ee421033be29cb1458671dd6f9298f3ee2044af0
Shivam <shivam.kumar@wayaheadtechnology.com>
2025-07-01 15:41:00 +0530

    add fileneat and kafka

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,10 +4,14 @@ from app.dao.job_index import index_job
 from app.api.v1.routes import health, agent_job, sub_agent_chain, agent_memory, test_agent_run
 from app.core.index_manager import IndexManager
 from app.core.elastic import get_es_client
+from app.core.core_log import logger
 from app.api.v1.routes import orchestrator
+
+logger.info("✅ EA-AURA backend logging initialized")
 
 app = FastAPI()
 
+# Include all API routers
 #Include all API routers
 
 app.include_router(health.router, prefix="/api/v1/health", tags=["Health"])
@@ -17,13 +21,17 @@ app.include_router(sub_agent_chain.router, prefix="/api/v1")
 app.include_router(agent_memory.router, prefix="/api/v1")
 app.include_router(test_agent_run.router, tags=["Debug"])
 
+# One startup event for both ES + Index init
 #One startup event for both ES + Index init
 @app.on_event("startup")
 async def startup_event():
     get_es_client()
     IndexManager.create_indices()
 
+# Root endpoint
 #Root endpoint
 @app.get("/")
 async def root():
     return {"message": "EA AURA AI Platform"}
+
+
